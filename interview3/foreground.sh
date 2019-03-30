36513e18398f55d13732d3ff5a8b7d825701a1b3
Anthony Sayre <anthony_sayre@msn.com>
2019-03-30 07:37:37 -0600

    add pip and git, git clone datadogpy and datadog ansible callback

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
+#!/bin/bash -eu
 apt-get --yes install ansible
+
+apt install -y git python-pip && git clone https://github.com/DataDog/datadogpy.git https://github.com/DataDog/ansible-datadog-callback.git
+
+chmod +x datadogpy/setup.py
+python datadogpy/setup.py install
+
+pip install pyyaml
