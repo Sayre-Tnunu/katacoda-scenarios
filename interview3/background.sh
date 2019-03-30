7787cd4c2db40791bf8263f94fb532806aa1651a
Anthony Sayre <anthony_sayre@msn.com>
2019-03-30 08:11:54 -0600

    change foreground.sh and background.sh

--- interview3/background.sh
+++ interview3/background.sh
@@ -2,7 +2,9 @@ apt-add-repository --yes --update ppa:ansible/ansible
 
 touch /root/test123
 
-apt-get install -y git python-pip && git clone https://github.com/DataDog/datadogpy.git; git clone https://github.com/DataDog/ansible-datadog-callback.git
+apt-get install -y git python-pip
+git clone https://github.com/DataDog/datadogpy.git
+git clone https://github.com/DataDog/ansible-datadog-callback.git
 cd datadogpy && chmod +x setup.py
 python setup.py install
 pip install pyyaml
--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -1,6 +1,2 @@
 #!/bin/bash -eu
 apt-get --yes install ansible
-apt-get install -y git python-pip && git clone https://github.com/DataDog/datadogpy.git; git clone https://github.com/DataDog/ansible-datadog-callback.git
-cd datadogpy && chmod +x setup.py
-python setup.py install
-pip install pyyaml
