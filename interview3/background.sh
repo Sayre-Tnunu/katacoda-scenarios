382e516d53333afe77de70f965dda460ae597e9f
Anthony Sayre <anthony_sayre@msn.com>
2019-03-30 08:22:27 -0600

    change background.sh

--- interview3/background.sh
+++ interview3/background.sh
@@ -1,10 +1,10 @@
 apt-add-repository --yes --update ppa:ansible/ansible
 
 touch /root/test123
-
+mkdir call_plugins
 apt-get install -y git python-pip
 git clone https://github.com/DataDog/datadogpy.git
 git clone https://github.com/DataDog/ansible-datadog-callback.git
 cd datadogpy && chmod +x setup.py
-python setup.py install
+python setup.py install && cd ..
 pip install pyyaml
