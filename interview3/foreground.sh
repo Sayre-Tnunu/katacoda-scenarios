01729a1992717cb399bf8d1ef65dbd98861fd33f
Anthony Sayre <anthony_sayre@msn.com>
2019-03-30 07:57:45 -0600

    change foreground

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -eu
 apt-get --yes install ansible
-
-apt install -y git python-pip && git clone https://github.com/DataDog/datadogpy.git https://github.com/DataDog/ansible-datadog-callback.git
-
-chmod +x datadogpy/setup.py
-python datadogpy/setup.py install
-
+apt install -y git python-pip && git clone https://github.com/DataDog/datadogpy.git; git clone https://github.com/DataDog/ansible-datadog-callback.git
+cd datadogpy && chmod +x setup.py
+python setup.py install
 pip install pyyaml
