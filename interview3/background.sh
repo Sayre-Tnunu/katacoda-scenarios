256a05b173d6625b5e916d8b0a5e03d00434c2b5
Anthony Sayre <anthony_sayre@msn.com>
2019-03-31 06:54:12 -0600

    latest adds

--- interview3/background.sh
+++ interview3/background.sh
@@ -1,10 +1,11 @@
 apt-add-repository --yes --update ppa:ansible/ansible
 
 touch /root/test123
-mkdir call_plugins
+mkdir callback_plugins
 apt-get install -y git python-pip
 git clone https://github.com/DataDog/datadogpy.git
 git clone https://github.com/DataDog/ansible-datadog-callback.git
 cd datadogpy && chmod +x setup.py
 python setup.py install && cd ..
 pip install pyyaml
+cp ansible-datadog-callback/datadog_callback.py call_plugins/
