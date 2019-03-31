d6785a6a64fd3ff86bb22aef87b5c590d1af1d35
Anthony Sayre <anthony_sayre@msn.com>
2019-03-31 07:50:06 -0600

    put it back like it was

--- interview3/background.sh
+++ interview3/background.sh
@@ -1,11 +1,11 @@
 apt-add-repository --yes --update ppa:ansible/ansible
 
 touch /root/test123
+mkdir callback_plugins
 apt-get install -y git python-pip
 git clone https://github.com/DataDog/datadogpy.git
 git clone https://github.com/DataDog/ansible-datadog-callback.git
-git clone git@github.com:Sayre-Tnunu/katacoda-scenarios.git
 cd datadogpy && chmod +x setup.py
 python setup.py install && cd ..
 pip install pyyaml
-cp ansible-datadog-callback/datadog_callback.py katacoda-scenarios/interview3/callback_plugins/
+cp ansible-datadog-callback/datadog_callback.py callback_plugins/
