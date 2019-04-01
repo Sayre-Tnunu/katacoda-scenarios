2dca949452b17fa9b61aa605e3421d54af71dd61
Anthony Sayre <anthony_sayre@msn.com>
2019-03-31 20:51:34 -0600

    export DD_API_KEY var

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
-apt-get --yes install ansible && \
+apt-get --yes install ansible curl && \
 export ANSIBLE_HOST_KEY_CHECKING=False && \
-apt install -y curl && \
 export api_key=471f90bc89bedc40f9878b013780d1e3 && \
+export DD_API_KEY=api_key && \
 #export DD_API_KEY=471f90bc89bedc40f9878b013780d1e3 && \
 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
