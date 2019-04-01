1c7b2a7ce889a2164d0f272f7e4924e436bb0b0a
Anthony Sayre <anthony_sayre@msn.com>
2019-03-31 18:01:54 -0600

    semi-colon

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -eu
-apt-get --yes install ansible, ansible datadog -i hosts --list | ./update-host-key.sh
+apt-get --yes install ansible; ansible datadog -i hosts --list | ./update-host-key.sh
