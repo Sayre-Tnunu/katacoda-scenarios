ef258dd8dc8eb5e38ebba69ea3bec25e85460a96
Anthony Sayre <anthony_sayre@msn.com>
2019-03-31 22:46:02 -0600

    this is totally unsecure

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -2,7 +2,7 @@
 apt-get --yes install ansible curl && \
 export ANSIBLE_DATADOG_CALLBACK_CONF_FILE=datadog_callback.yml && \
 export ANSIBLE_HOST_KEY_CHECKING=False && \
-#export api_key=xxxxxx && \
+export api_key=c4d19951ee0cd9b32e489c7a8d5fccc6 && \
 export DD_API_KEY=$api_key && \
 #export DD_API_KEY=471f90bc89bedc40f9878b013780d1e3 && \
 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
