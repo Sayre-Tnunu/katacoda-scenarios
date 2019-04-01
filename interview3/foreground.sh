215d718ddc1857b8dac495b5c39ba51f13f636f6
Anthony Sayre <anthony_sayre@msn.com>
2019-03-31 19:52:47 -0600

    add dd api key

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -eu
 apt-get --yes install ansible && \
-export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_HOST_KEY_CHECKING=False && \
+apt install -y curl && \
+DD_API_KEY=471f90bc89bedc40f9878b013780d1e3 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
