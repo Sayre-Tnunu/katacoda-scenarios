c26980c010c52dc3a7b4e7f4b7f06f896a0df4f9
Anthony Sayre <anthony_sayre@msn.com>
2019-03-31 18:10:25 -0600

    chmod file before running

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -eu
-apt-get --yes install ansible; ansible datadog -i hosts --list | ./update-host-key.sh
+apt-get --yes install ansible && \
+chmod +x update-host-key.sh && \
+ansible datadog -i hosts --list | ./update-host-key.sh
