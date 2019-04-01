51994f989b8d88e3f63521f935f9fdabffde4d99
Anthony Sayre <anthony_sayre@msn.com>
2019-03-31 21:49:45 -0600

    ansible-vault encrypted datadog_callback.yml

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -2,7 +2,7 @@
 apt-get --yes install ansible curl && \
 export ANSIBLE_DATADOG_CALLBACK_CONF_FILE=datadog_callback.yml && \
 export ANSIBLE_HOST_KEY_CHECKING=False && \
-export api_key=471f90bc89bedc40f9878b013780d1e3 && \
+#export api_key=xxxxxx && \
 export DD_API_KEY=$api_key && \
 #export DD_API_KEY=471f90bc89bedc40f9878b013780d1e3 && \
 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
