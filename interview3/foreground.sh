293d7ebbb2294a2b0d8808285ee6b7284a113988
Anthony Sayre <anthony_sayre@msn.com>
2019-04-01 23:23:29 -0600

    put vars back in foreground.sh

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -eu
 apt-get --yes install ansible curl && \
+ansible-vault decrypt datadog_callback.yml && \
 export ANSIBLE_HOST_KEY_CHECKING=False && \
-ansible-vault decrypt datadog_callback.yml
+export ANSIBLE_DATADOG_CALLBACK_CONF_FILE=datadog_callback.yml && \
+export api_key=$api_key && \
+export DD_API_KEY=$api_key && \
+bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
