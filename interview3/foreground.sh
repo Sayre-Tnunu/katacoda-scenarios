b0bd17752057c21d3c6a422d0fb8e1c1af4a68b6
Anthony Sayre <anthony_sayre@msn.com>
2019-04-01 23:35:42 -0600

    rmeove trailing && /

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -eu
-apt-get --yes install ansible curl && \
 ansible-vault decrypt datadog_callback.yml && \
 export ANSIBLE_HOST_KEY_CHECKING=False && \
 export ANSIBLE_DATADOG_CALLBACK_CONF_FILE=datadog_callback.yml && \
-export api_key=$api_key && \
-export DD_API_KEY=$api_key && \
-bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
+export api_key=$ANSIBLE_DATADOG_CALLBACK_CONF_FILE && \
+export DD_API_KEY=$api_key
+#apt-get --yes install ansible curl && \
+#bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
