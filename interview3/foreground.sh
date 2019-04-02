c0c0fe68ae9d7f3e56f81c28cf1e7145622e3f87
Anthony Sayre <anthony_sayre@msn.com>
2019-04-02 00:29:15 -0600

    comment out trailing && \

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -6,5 +6,5 @@ source api_key && \
 export ANSIBLE_DATADOG_CALLBACK_CONF_FILE=datadog_callback.yml && \
 #export api_key=$api_key && \
 export DD_API_KEY=$api_key
-export ANSIBLE_HOST_KEY_CHECKING=False && \
+export ANSIBLE_HOST_KEY_CHECKING=False #&& \
 #bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
