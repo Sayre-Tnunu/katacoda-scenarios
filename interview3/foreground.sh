89677f5eb20eab9ab2dcbba366e7626d7407a0f5
Anthony Sayre <anthony_sayre@msn.com>
2019-04-02 00:40:25 -0600

    api_key var attempt #2

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -eu
-#apt-get --yes install ansible curl && \
+apt-get --yes install ansible curl && \
 ansible-vault decrypt api_key && \
-set -o allexport && \
-source api_key && \
+set -o allexport && source api_key && \
 export ANSIBLE_DATADOG_CALLBACK_CONF_FILE=datadog_callback.yml && \
 #export api_key=$api_key && \
 export DD_API_KEY=$api_key
