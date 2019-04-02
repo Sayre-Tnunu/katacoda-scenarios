cf34643d4d5489aa13058800155563d20358bbd3
Anthony Sayre <anthony_sayre@msn.com>
2019-04-02 00:44:04 -0600

    turn datadog launch back on

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -3,7 +3,6 @@ apt-get --yes install ansible curl && \
 ansible-vault decrypt api_key && \
 set -o allexport && source api_key && \
 export ANSIBLE_DATADOG_CALLBACK_CONF_FILE=datadog_callback.yml && \
-#export api_key=$api_key && \
 export DD_API_KEY=$api_key
 export ANSIBLE_HOST_KEY_CHECKING=False #&& \
-#bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
+bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
