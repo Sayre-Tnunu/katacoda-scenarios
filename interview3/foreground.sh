b2038d1310b997b070944434070da627701b1c77
Anthony Sayre <anthony_sayre@msn.com>
2019-04-02 00:26:18 -0600

    encrypt api_key file instead

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eu
-apt-get --yes install ansible curl && \
-ansible-vault decrypt datadog_callback.yml && \
-export ANSIBLE_HOST_KEY_CHECKING=False && \
+#apt-get --yes install ansible curl && \
+ansible-vault decrypt api_key && \
+set -o allexport && \
+source api_key && \
 export ANSIBLE_DATADOG_CALLBACK_CONF_FILE=datadog_callback.yml && \
-export api_key=$ANSIBLE_DATADOG_CALLBACK_CONF_FILE && \
+#export api_key=$api_key && \
 export DD_API_KEY=$api_key
+export ANSIBLE_HOST_KEY_CHECKING=False && \
 #bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
