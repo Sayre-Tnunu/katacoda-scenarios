32d7e7861c0ca739757a6879acc9714d60cbf86c
Anthony Sayre <anthony_sayre@msn.com>
2019-04-01 22:21:55 -0600

    export shell commands now in ansible

--- interview3/background.sh
+++ interview3/background.sh
@@ -5,8 +5,9 @@ mkdir callback_plugins
 apt-get install -y git python-pip
 git clone https://github.com/DataDog/datadogpy.git
 git clone https://github.com/DataDog/ansible-datadog-callback.git
+#git clone git@gitlab.com:asayre/datadog-ansible-vault.git
+
 cd datadogpy && chmod +x setup.py
 python setup.py install && cd ..
 pip install pyyaml
 cp ansible-datadog-callback/datadog_callback.py callback_plugins/
-ansible datadog -i hosts --list | ./update-host-key.sh
