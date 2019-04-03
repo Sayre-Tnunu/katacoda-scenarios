ea2fd1c572f0bc9a6443fc449fbe90776e1a6888
Anthony Sayre <anthony_sayre@msn.com>
2019-04-02 18:44:28 -0600

    fix chmods2

--- interview3/background.sh
+++ interview3/background.sh
@@ -7,7 +7,8 @@ git clone https://github.com/DataDog/datadogpy.git
 git clone https://github.com/DataDog/ansible-datadog-callback.git
 #git clone git@gitlab.com:asayre/datadog-ansible-vault.git
 
-cd datadogpy && chmod +x setup.py && chmod +x launch.sh
+cd datadogpy && chmod +x setup.py
+chmod +x launch.sh
 python setup.py install && cd ..
 pip install pyyaml
 cp ansible-datadog-callback/datadog_callback.py callback_plugins/
