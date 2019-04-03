f5dfbecdc20fc2b0affeade30fc1e34d35ef4def
Anthony Sayre <anthony_sayre@msn.com>
2019-04-02 18:52:49 -0600

    chmod +x launch.sh moved to foreground.sh

--- interview3/foreground.sh
+++ interview3/foreground.sh
@@ -3,4 +3,5 @@ apt-get --yes install ansible curl && \
 #ansible-vault decrypt api_key && \
 #set -o allexport && source api_key && \
 export ANSIBLE_HOST_KEY_CHECKING=False && \
-export ANSIBLE_DATADOG_CALLBACK_CONF_FILE=datadog_callback.yml #&& \
+export ANSIBLE_DATADOG_CALLBACK_CONF_FILE=datadog_callback.yml && \
+chmod +x launch.sh
